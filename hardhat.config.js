18f2595d47430befdfa02a5b313d9315378b84b7
Muhtasim Fahim <muhtasim.fahim.98@gmail.com>
2022-04-15 22:19:55 +0600

    deploy contract to rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,16 @@
+require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
 
 module.exports = {
   solidity: "0.8.7",
   defaultNetwork: "rinkeby",
   networks: {
-    rinkeby:{
-      hardhat: {},
-      url: process.env.MY_ALCHEMY_URL,
-      accounts: [`0x${process.env.MY_METAMASK_PRIVATE_KEY}`]
+    hardhat: {
     },
+    rinkeby: {
+      url: process.env.MY_ALCHEMY_URL,
+      accounts: [process.env.MY_METAMASK_PRIVATE_KEY]
+    }
   },
 };
